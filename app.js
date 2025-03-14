0f393526c0d792fe5b4609ac99296723f3d42b67
Helina Jrvsr <helina.jarvesaar@gmail.com>
2025-03-14 14:27:50 +0200

    save app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+const btnHamburger = document.querySelector('.hamburger');
+const menuList = document.querySelector('.menu-list');
+const menuLinks = document.querySelectorAll('.menu-list a');
+
+btnHamburger.addEventListener('click', () => {
+    menuList.classList.toggle('active');
+})
+
+document.addEventListener('click', (event) => {
+    if (!menuList.contains(event.target) && !btnHamburger.contains(event.target)) {
+        menuList.classList.remove('active');
+    }
+})
+
+menuLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        menuList.classList.remove('active');
+    })
+})
+
+const frameImage = document.querySelector('#frame');
+const images = ['1.webp', '2.webp', '3.jpeg', '4.jpeg', '5.webp', '6.jpeg'];
+let currentImageIndex = 0;
+
+document.addEventListener('click', () => {
+    currentImageIndex++;
+
+    if (currentImageIndex >= 6) {
+        currentImageIndex = 0;
+    }
+    frameImage.src = 'img/' + images[currentImageIndex];
+})
